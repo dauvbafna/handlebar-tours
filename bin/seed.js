3770eedf2772dee0035df29ee993b6e01f2b7632
lenniez <lenniezhu@gmail.com>
2018-04-24 00:12:27 +0200

    homepage css and html improved

--- bin/seed.js
+++ bin/seed.js
@@ -10,8 +10,8 @@ mongoose.connect('mongodb://localhost/moto-tours', {
   reconnectTries: Number.MAX_VALUE
 })
   .then(() => {
-    const fullroute = [{coordinates:[27.7090319,85.2911132]},{coordinates:[28.2298558,83.8865779]},{coordinates:[28.6378237,83.5971574]},{coordinates:[28.8009381,83.7770043]},{coordinates:[28.4918295,83.579959]},{coordinates:[28.2298558,83.8865779]},,{coordinates:[27.7090319,85.2911132]}];
-    return createNewTour('7 days wonderful motorbike tour in nepal', 'some cool description here',fullroute);
+    const fullroute = [{coordinates: [27.7090319, 85.2911132]}, {coordinates: [28.2298558, 83.8865779]}, {coordinates: [28.6378237, 83.5971574]}, {coordinates: [28.8009381, 83.7770043]}, {coordinates: [28.4918295, 83.579959]}, {coordinates: [28.2298558, 83.8865779]}, {coordinates: [27.7090319, 85.2911132]}];
+    return createNewTour('7 days wonderful motorbike tour in nepal', 'some cool description here', fullroute);
   })
   .then(() => {
     mongoose.disconnect();
@@ -21,10 +21,10 @@ mongoose.connect('mongodb://localhost/moto-tours', {
     mongoose.disconnect();
   });
 
-function createNewTour (tripname, tripdescription, triproutes) {
-  const name = tripname; 
-  const description = tripdescription ;
-  const routes = triproutes;
+function createNewTour (tripName, tripDescription, tripRoutes) {
+  const name = tripName;
+  const description = tripDescription;
+  const routes = tripRoutes;
 
   const tour = new Tour({
     name,
