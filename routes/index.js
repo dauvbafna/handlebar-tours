a48a5b1e8f14a3f4bc15349cb1bf359d3e903df8
lenniez <lenniezhu@gmail.com>
2018-04-24 16:28:17 +0200

    updated CSS, without dauvs google maps work

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,18 @@
 const express = require('express');
 const router = express.Router();
 const Tour = require('../models/tour');
+const User = require('../models/user');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  Tour.find({})
+    .then((result) => {
+      const data = {
+        tour: result
+      };
+      res.render('index', data);
+    })
+    .catch(next);
 });
 
 /* GET tour details page */
@@ -17,7 +25,19 @@ router.get('/tours/:tourId', (req, res, next) => {
         tour: result
       };
       res.render('tours-detail', data);
-    });
+    })
+    .catch(next);
+});
+
+router.get('/profile/:userId', (req, res, next) => {
+  User.findOne({ _id: req.params.userId })
+    .then((result) => {
+      const data = {
+        user: result
+      };
+      res.render('profile', data);
+    })
+    .catch(next);
 });
 
 module.exports = router;
